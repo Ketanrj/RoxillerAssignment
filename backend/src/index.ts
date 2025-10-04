384591a0f2d7ae3d7f4b495983f5e7c74227240b
Ketan Jadhav <ketanrj@users.noreply.github.com>
2025-10-04 19:52:28 +0530

    updated index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,32 +1,29 @@
-import { type Express } from 'express'
-import express from 'express'
-import { PORT } from './secrets';
-import cors from 'cors'
-import corsOptions from './config/corsOptions';
-import  cookieParser from 'cookie-parser'
-import { Rootrouter } from './routes';
-import { PrismaClient } from '@prisma/client';
-import credentials from './middlewares/credentials';
+import { type Express } from "express";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { Rootrouter } from "./routes"; // adjust path
+import { PrismaClient } from "@prisma/client";
+import credentials from "./middlewares/credentials";
+import corsOptions from "./config/corsOptions";
+import serverless from "serverless-http";
 
 const app: Express = express();
 
 export const prismaClient = new PrismaClient({
-    log: ['query']
-})
+  log: ["query"],
+});
 
 app.use(credentials);
-
-app.use(cors(corsOptions))
-
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.json());
-
 app.use(cookieParser());
 
-app.use('/api', Rootrouter);
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Express on Vercel" });
+});
 
+app.use("/api", Rootrouter);
 
-app.listen(PORT, () => {
-    console.log("Server Started on port 3000")
-})
+export default serverless(app);
